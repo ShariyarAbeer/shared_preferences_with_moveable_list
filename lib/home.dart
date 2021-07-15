30a5d6bb5fd8eceaf8a7fec7e98f8c31bb76d35b
ShariyarAbeer <007abeershariyar@gmail.com>
2021-07-15 15:41:13 +0600

    up

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
 import 'saveData.dart';
 
 class Home extends StatefulWidget {
@@ -10,6 +10,7 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  String give;
   List<String> item = [
     "abeer",
     "sadil",
@@ -29,28 +30,66 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("data"),
       ),
-      body: ListView.builder(
-        itemCount: item == null ? 0 : item.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(item[index]),
-            trailing: IconButton(
-              onPressed: () {
-                name.add(item[index]);
-                print(name);
-              },
-              icon: Icon(Icons.add),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: item == null ? 0 : item.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(item[index]),
+                    trailing: IconButton(
+                      onPressed: () {
+                        name.add(item[index]);
+
+                        print(name);
+                      },
+                      icon: Icon(Icons.add),
+                    ),
+                  );
+                },
+              ),
             ),
-          );
-        },
+            Expanded(
+              child: ListView.builder(
+                itemCount: name == null ? 0 : name.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(name[index]),
+                    // trailing: IconButton(
+                    //   onPressed: () {
+                    //     name.add(item[index]);
+
+                    //     print(name);
+                    //   },
+                    //   icon: Icon(Icons.add),
+                    // ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.blue,
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SavaData()));
-        },
-      ),
+          backgroundColor: Colors.blue, onPressed: _save
+          // () {
+          // Navigator.push(
+          //     context, MaterialPageRoute(builder: (context) => SavaData()));
+          //},
+          ),
     );
+  }
+
+  _save() async {
+    // List<int> kj = [1, 2, 3, 4];
+    List<String> myListOfStrings = name.map((i) => i.toString()).toList();
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    List<String> myList = (prefs.getStringList('mylist') ?? List<String>());
+    List<int> myOriginaList = myList.map((i) => int.parse(i)).toList();
+    print('Your list  $myOriginaList');
+    await prefs.setStringList('mylist', myListOfStrings);
   }
 }
